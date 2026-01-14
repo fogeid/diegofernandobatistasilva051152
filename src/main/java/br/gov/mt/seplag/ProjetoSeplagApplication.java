435713fdb8d25171902bc9049d16c9639da08719
Diego Batista <diegobatista@MacBook-Air-de-Diego.local>
2026-01-14 14:04:13 -0300

    Projeto inicial estrutural

---
+++ src/main/java/br/gov/mt/seplag/ProjetoSeplagApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSeplagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSeplagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/seplag/ProjetoSeplagApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSeplagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
